3b73bff2111f328ca11f9c7dc6e2ef5b7e946554
matmaslanka <mates94@gmail.com>
2024-02-19 21:30:44 +0100

    commit 3 great place to go further

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('<int:post>', views.post_content_by_number),
     path('<str:post>', views.post_content,name='post-content')
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,19 +4,26 @@ from django.urls import reverse
 
 all_posts = {
     'post1': 'Praca w django to czysta przyjemność!',
+    'post2': 'Wspinaczka to świetna rzecz!',
+    'post3': None
 }
 
 
 def index(request):
-    return HttpResponse("Its ok!")
+    list_items = ''
+    posts = list(all_posts.keys())
+
+    return render(request, "blog/index.html", {
+        'posts': posts
+    })
 
 
 def post_content_by_number(requst, post):
     posts = list(all_posts.keys())
 
     if post > len(posts):
-        return HttpResponseNotFound('Invalid month')
-    
+        return HttpResponseNotFound('Invalid post number')
+
     redirect_post = posts[post - 1]
     redirect_path = reverse('post-content', args=[redirect_post])
     return HttpResponseRedirect(redirect_path)
@@ -25,6 +32,9 @@ def post_content_by_number(requst, post):
 def post_content(request, post):
     try:
         post_text = all_posts[post]
+        return render(request, "blog/post.html", {
+            'post_name': post,
+            'text': post_text
+        })
     except:
         raise Http404()
-    return HttpResponse(post_text)
